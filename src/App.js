c3ebe0d99f8729c4f47d0c9456bd8a2ff9e06b33
falanorajib <53591966+falanorajib@users.noreply.github.com>
2021-11-29 16:33:33 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import "assets/scss/style.css";
+function App() {
+    return ( <
+        div className = "App" >
+        hai dunia!
+        </div>
+    );
+}
+
+export default App;
---
+++ src/pages/LandingPage.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import moduleName from "elements/Button";
+
+function LandingPage() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default LandingPage
